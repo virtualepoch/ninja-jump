c4b6cd5f06e8a4f624a87b9814caffeb8be71b98
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-03-26 16:12:14 -0600

    ..

--- main.js
+++ main.js
@@ -5,18 +5,19 @@ function jump() {
   if (character.classList.contains("animate")) {
     return;
   }
-
   character.classList.add("animate");
   setTimeout(function () {
     character.classList.remove("animate");
   }, 500);
 }
 
-var checkDead = setInterval(function () {
+setInterval(checkDead, 10);
+
+function checkDead() {
   var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
   var blockLeft = parseInt(window.getComputedStyle(enemy).getPropertyValue("left"));
-
   if (blockLeft < 14 && blockLeft > 10 && characterTop >= 42) {
-    
+    alert("GAME OVER");
+    enemy.style.animation = "none";
   }
-}, 10);
+}
